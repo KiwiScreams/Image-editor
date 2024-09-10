6fab4663c7bfe9374d9be98025d186f814d0a9af
KiwiScreams <screamskiwi3@gmail.com>
2024-09-10 15:40:49 +0400

    drag and drop

--- script/app.js
+++ script/app.js
@@ -6,4 +6,16 @@ function uploadImage()
 {
     let imgLink = URL.createObjectURL(inputFile.files[0]);
     imageView.style.backgroundImage = `url(${imgLink})`;
+    imageView.textContent = "";
+    imageView.style.border = 0;
 }
+dropArea.addEventListener("dragover", function(e)
+{
+    e.preventDefault();
+});
+dropArea.addEventListener("drop", function(e)
+{
+    e.preventDefault();
+    inputFile.files = e.dataTransfer.files;
+    uploadImage();
+});

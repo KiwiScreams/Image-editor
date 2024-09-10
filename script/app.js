5575724c8c13d5179872a26749bea53d57bb5433
KiwiScreams <screamskiwi3@gmail.com>
2024-09-10 17:34:30 +0400

    responsive design

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
-const imageView = document.getElementById("image-view"); // Move this to the top
+const imageView = document.getElementById("image-view");
 const grayscaleSlider = document.getElementById("grayscale-slider");
 const opacitySlider = document.getElementById("opacity-slider");
 const blurSlider = document.getElementById("blur-slider");
